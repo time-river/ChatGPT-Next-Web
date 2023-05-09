d02fc5a48961b9a15b9b30b34d9aaf24ef10ad7d
river <river@vvl.me>
2023-05-09 22:45:49 +0800

    config: change username and password limit
    Signed-off-by: river <river@vvl.me>

--- global.config.js
+++ global.config.js
@@ -4,7 +4,7 @@ module.exports = {
     domainFrom: "2023",
     recaptchaKey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
     user: {
-        usenamePattern: "^\\S{4,}$",
+        usenamePattern: "^\\S{4,16}$",
         emailPattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
         passwdPattern: "^.{6,}$",
     },
